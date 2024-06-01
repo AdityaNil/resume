23639cd1666b207414a59b53c7380950873acfd4
AdityNil <sirf_aadi@outlook.in>
2024-06-01 19:00:11 +0530

    hello

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,46 @@
 import React from 'react'
-
+import conta from '../assets/conta.jpg'
 const Contact = () => {
   return (
-    <div>
-      contact
+    <div style={styles.contact}>
+      <h1 style={{ textAlign: 'center', fontSize: '50px' }}><u>Contact</u></h1>
+      <div>
+        <form style={{display:'grid',paddingLeft:"40rem",paddingTop:"4rem"}}>
+          <input style={styles.input} type="text" placeholder='Name'></input>
+          <input style={styles.input} type="mail" placeholder='Email'></input>
+          <textarea style={styles.input} name="Text Here..." cols="0" rows="10"></textarea>
+          <input style={styles.button} type="button" value="Send"></input>
+        </form>
+      </div>    
     </div>
   )
+}
+
+const styles={
+  contact:{
+    backgroundImage: `url(${conta})`,
+    paddingBottom: '93px',
+    fontSize: '25px',
+    backgroundSize: 'cover', 
+    backgroundPosition: 'center',
+    height: '74.5vh', 
+    marginTop:'-25px'
+  },
+  input:{
+    width:'25%',
+    padding:'1rem',
+    borderRadius:'.5rem',
+    border:'2px solid green',
+    resize:'none',
+    marginBottom:'1rem'
+  },
+  button:{
+    olor:'black',
+    width:'29%',
+    fontSize:'25px',
+    cursor: 'pointer',
+    borderRadius:'10px',
+  }
 }
 
 export default Contact
--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import educate from '../assets/educate.png'
 
 const Education = () => {
   return (
-    <div>
-      Education
+    <div style={{backgroundColor: "#FEFAF6",marginTop: '-25px',marginBottom:'25px'}}>
+      <h1 style={{ textAlign: 'center', fontSize: '50px' }}><u>Education</u></h1>
+      <div style={{display:'inline-flex',paddingLeft:'25vh',gap:'20vh'}}>
+        <div>
+            <img src={educate} alt='loading' />
+        </div>
+        <div >
+          <h2>COLLEGE/SCHOOL</h2>
+          <ul>
+          <li><h3 style={{color:'red'}}>Chandigarh University</h3><span>May 2021 - Present</span> <br/> Bachelor of Engineering  8.04 CGPA</li>
+          <li><h3 style={{color:'red'}}>Lucknow Public School & College</h3><span>Mar 2020 - Apr 2021</span><br/> Intermediate  81.2%</li>
+          <li><h3 style={{color:'red'}}>St.Xavier's School</h3><span>Mar 2018 - Apr 2019</span><br/> Matriculation  81.3%</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
